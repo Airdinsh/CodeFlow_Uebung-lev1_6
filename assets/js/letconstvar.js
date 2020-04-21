68267b2dfd9af7df4d399a874530fb93237f5ef4
airdinsh <airdinsh@icloud.com>
2020-04-21 17:42:04 +0200

    Second commit

---
+++ assets/js/letconstvar.js
@@ -0,0 +1,143 @@
+/* Code Flow Übung 1_6 */
+
+/* var z = 34;
+z = 67
+console.log(z); */
+
+/* let x = 34;
+x = 67;
+console.log(x); */
+
+// die Variablen let und var können sich nach neuer definition oder berechnungen des wertes etc ändern
+
+/* const y = 34;
+y = 67;
+console.log(y); */
+// eine Konstante kann nicht verändert werden
+
+
+
+/* Code Flow Übung 1_8 arithmetic operators */
+
+let x = 20; 
+let y = 30;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+let z = 10;
+
+console.log(x * y);
+
+let resultOne = x * y / z;
+//oder 
+console.log(resultOne = x * y / z);
+// dann würde die nächste Zeile unnötig sein
+console.log(resultOne);
+
+
+
+
+
+
+
+let a = 15;
+let b = 9;
+console.log(a % b);
+
+let c = 20;
+
+
+let resultTwo = (a + b) * c;
+
+console.log(resultTwo);
+
+a--;
+
+console.log(a);
+
+--b;
+
+console.log(b);
+
+let resultthree = a - b;
+
+console.log(resultthree);
+
+console.log(resultOne % resultTwo );
+
+
+
+
+
+/* Code Flow Übung Lev1_12_js-einführung_arithmetic_operators 8.1_js-intro */
+
+
+var score = 5 + 5 * 10;
+console.log('Ergebnis: ' + score);
+
+
+var score = 5 + 5 * 10;
+console.log('Ergebnis: ' + score);
+
+var score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score);
+
+score += 10;
+console.log('Ergebnis: ' + score);
+
+
+
+
+var increment = 1;
+increment = increment + 1;
+increment += 1;
+
+increment++;
+console.log('increment: ' + increment);
+
+increment--;
+console.log('increment: ' + increment);
+
+
+var additionOperator = 1 + 1;
+console.log("addition: " + additionOperator);
+
+
+/* Code Flow Übung Lev1_13_js-einführung_arithmetische_wiederholung 8.1_js-intro */
+// Verwende Grundrechenarten
+// Addition
+
+var addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+var subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+var multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+// Division
+var division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest sein würde.
+var modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
+
+
+
+
+
+
+
+
+
